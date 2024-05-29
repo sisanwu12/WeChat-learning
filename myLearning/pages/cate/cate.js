73b520237c8f7048d428567a8f71f7dffc847e9f
肆叁伍12 <oncwnuN00TDJEV3Cfau70oEXoEkc@git.weixin.qq.com>
2024-05-29 22:06:39 +0800

    #

--- myLearning/pages/cate/cate.js
+++ myLearning/pages/cate/cate.js
@@ -1,66 +1,61 @@
 // pages/cate/cate.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // 小程序页面中所需要使用的数据均来自data对象
+  data:{
+    school:"安信工",
+    obj:{
+      name:"tom",
+      age:"18"
+    },
+    num: 1,
+    user:{},
+    value: "123",
+    isChecked: flase
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  // 更新 num
+  updataNUM() {
+    // 更新数据并驱动视图更新
+    this.setData({
+      num:this.data.num+1
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+  // 更新 user
+  updateUSER(){
+    this.setData({
+      // 如果给对象新增属性，可以将 key 写成数据路径的方式
+      "user.name":"tom",
+      "user.age": 19
+    })
 
+    // ES6 提供的展开运算符更改数据
+    // const user = {
+    //   ...this.data.user,
+    //   name:"jerry",
+    //   age:"8"
+    // }
+
+    // object.assign() 将多个对象合并为一个对象
+    const user = Object.assign(this.data.user,{name:"jerry"},{age:"8"})
+
+    this.setData({
+      user:user
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+  // 删除 user属性
+  deleteUSER(){
+    delete this.data.user.age
+    this.setData({
+      user:this.data.user
+    })
 
-  },
+    // rest 剩余参数
+    const {age,...rest} = this.data.user
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    this.setData({
+      user:rest
+    })
   }
 })
--- myLearning/pages/login/login.js
+++ myLearning/pages/login/login.js
@@ -1,4 +1,4 @@
-// pages/cart/cart.js
+// pages/login.js
 Page({
 
   /**
